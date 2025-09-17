5a663ef6460501c799be6bb367a5ad5144780aad
Urbesh Mondal <161818822+Urbesh@users.noreply.github.com>
2025-09-17 12:10:08 +0530

    Delete Phase1_Fundamentals/Week 1/Day2/D2P2.py

--- Phase1_Fundamentals/Week 1/Day2/D2P2.py
+++
@@ -1,9 +0,0 @@
-#Check if a number is greater than another, and if they are equal.
-n1=float(input("Enter First Number: "))
-n2=float(input("Enter Sercond Number: "))
-if n1>n2:
-    print(n1,"is Greater than",n2)
-elif n1<n2:
-    print(n2,"is Greater than",n1)
-else:
-    print(n1,"and",n2,"are equal")
