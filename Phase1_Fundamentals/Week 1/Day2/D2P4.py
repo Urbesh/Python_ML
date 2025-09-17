cde24f3eee0d91ae29731e587bc12e33ff295a83
Urbesh Mondal <161818822+Urbesh@users.noreply.github.com>
2025-09-17 12:09:50 +0530

    Delete Phase1_Fundamentals/Week 1/Day2/D2P4.py

--- Phase1_Fundamentals/Week 1/Day2/D2P4.py
+++
@@ -1,8 +0,0 @@
-#Use membership operators: Check if "a" is in "Shoyo"
-#I made the program dynamic so the it can accept more words, rather than just the single word "Shoyo"
-wod=input("Enter a Word or a Sentence: ")
-fid=input("Enter the letter or the word you want to check is it's present or not: ")
-if fid in wod:
-    print(fid,"is Present")
-else:
-    print(fid,"is not present")
