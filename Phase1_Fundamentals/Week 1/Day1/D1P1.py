4f4a01642aa92f2d3d8cf1b9225f0d3716b7f20f
Urbesh Mondal <161818822+Urbesh@users.noreply.github.com>
2025-09-17 12:07:54 +0530

    Delete Phase1_Fundamentals/Week 1/Day1/D1P1.py

--- Phase1_Fundamentals/Week 1/Day1/D1P1.py
+++
@@ -1,6 +0,0 @@
-#Write a script to take user input and print type of each variable.
-name=input("Enter your name: ")
-age=int(input("Enter your age: "))
-height=float(input("Enter your height in centimeters: "))
-print("Hello, my name is", name + ", I am", age, "years old and my height is", height, "cm.")
-print("Data types are:", type(name), type(age), type(height))
