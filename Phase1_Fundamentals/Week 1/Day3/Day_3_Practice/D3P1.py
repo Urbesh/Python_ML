a0af167f47c6c9aa27dfa6321512df672f82c046
Urbesh Mondal <urbeshmondal@gmail.com>
2025-09-17 13:07:49 +0530

    Added Day 3 Program 2

---
+++ Phase1_Fundamentals/Week 1/Day3/Day_3_Practice/D3P1.py
@@ -0,0 +1,5 @@
+#Basic String I/O: Input a name, print it in uppercase, lowercase, and title case.
+name = input("Enter your name: ")
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("Title Case:", name.title())
---
+++ Phase1_Fundamentals/Week 1/Day3/Day_3_Practice/D3P2.py
@@ -0,0 +1,4 @@
+#Reverse a String Input: "Shoyo" → Output: "oyohS".
+name = input("Enter your name: ")
+reversed_name = name[::-1]
+print("Reversed Name:", reversed_name)
