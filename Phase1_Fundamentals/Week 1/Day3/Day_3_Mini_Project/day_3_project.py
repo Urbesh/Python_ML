6b5cad128dac5861df435ba77862115c7078f4b2
Urbesh Mondal <urbeshmondal@gmail.com>
2025-09-18 21:53:07 +0530

    Just removed the comment line in day 3 project

--- Phase1_Fundamentals/Week 1/Day3/Day_3_Mini_Project/day_3_project.py
+++ Phase1_Fundamentals/Week 1/Day3/Day_3_Mini_Project/day_3_project.py
@@ -36,6 +36,5 @@ def string_analyzer():
     print(f"Word count: {word_count}")
     print(f"Reversed string: {reversed_sentence}")
     print(f"Is palindrome: {'Yes' if is_palindrome else 'No'}")
-# Call the function when the script is run
 if __name__ == "__main__":
     string_analyzer()
