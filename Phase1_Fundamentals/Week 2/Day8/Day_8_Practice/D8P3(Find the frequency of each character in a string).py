a5fd9aba162e28f4132750d8a33fdc62c76b7a23
Urbesh Mondal <urbeshmondal@gmail.com>
2025-09-22 17:27:09 +0530

    Made the practice program cleaner

--- Phase1_Fundamentals/Week 2/Day8/Day_8_Practice/D8P3(Find the frequency of each character in a string).py
+++ Phase1_Fundamentals/Week 2/Day8/Day_8_Practice/D8P3(Find the frequency of each character in a string).py
@@ -1,13 +1,10 @@
 #Find the frequency of each character in a string
 count={}
-letters=[]
 word=input("Enter a word: ")
-for i in word:
-    letters.append(i)
-for j in letters:
-    keys=j
-    alph_count=letters.count(j)
-    values=alph_count
-    count[keys]=values
+for ch in word:
+    if ch in count:
+        count[ch]+=1
+    else:
+        count[ch]=1
 print(f"The number of times each letter appeared in the word, {word}: ")
 print(count)
