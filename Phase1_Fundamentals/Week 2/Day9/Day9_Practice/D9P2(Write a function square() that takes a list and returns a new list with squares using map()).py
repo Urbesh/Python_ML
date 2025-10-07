8566e52827b1dbaf9b23c3d0fca15696c4209a62
Urbesh Mondal <urbeshmondal@gmail.com>
2025-10-07 14:59:30 +0530

    added second program of day 9

---
+++ Phase1_Fundamentals/Week 2/Day9/Day9_Practice/D9P2(Write a function square() that takes a list and returns a new list with squares using map()).py
@@ -0,0 +1,14 @@
+#Write a function square() that takes a list and returns a new list with squares using map()
+from math import pow
+def square(n):
+    return lambda a:pow(a,n)
+user_input=input("Enter the as many number as you want separated by a space: ")
+user_Str_input=user_input.split()
+#since we can't directly take integer type value, we will convert it to integers.
+num_list=list(map(int,user_Str_input))
+my_square=square(2)
+Square_num=[]
+for i in num_list:
+    elements=my_square(i)
+    Square_num.append(elements)
+print(f"The new list after performing square operation: {Square_num}")
